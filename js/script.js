8d44d6a268af9ba04ae5ab25d1ae913a56c3e0da
lucianocretella95 <luciano.cretella@tim.it>
2024-05-02 13:21:14 +0200

    M1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+const griglia = document.getElementById("grid");
 for (let i = 1; i <= 100; i++) {
-  const griglia = document.getElementById("grid");
   const element = `<div class="square ${i}">${i}</div>`;
   griglia.innerHTML += element;
 }
